6bb490ef721cf8bf7e0c655b75409cf324aad67e
Shahmir <sham9871@gmail.com>
2023-11-05 08:58:29 +0800

    fixed favicon link and added GA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-EXTVK4E2ZW'
+      }
+    })
+  ],
   base: '/todo_app/'
 })
